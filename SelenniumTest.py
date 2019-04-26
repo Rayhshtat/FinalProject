aa7ca4caca4cc99f6a6b84f55ddf8a719c68dfa9
Rayhshtat <48656196+Rayhshtat@users.noreply.github.com>
2019-04-27 00:00:27 +0300

    selenium script

---
+++ SelenniumTest.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+web_site = "http://192.168.99.100:5000/"
+driver = webdriver.Chrome(executable_path="chromedriver.exe")
+# default timeout of 10 sec
+driver.implicitly_wait(10)
+# open website
+driver.get(web_site)
+print(driver.find_element_by_xpath('//body[contains(text(), "I have been seen")]').text)
+driver.quit()
